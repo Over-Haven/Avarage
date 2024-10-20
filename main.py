8a70ff268c455b2654e49ee000f7ebd6f3beab79
Over-haven <tanushdas26@gmail.com>
2024-10-20 20:33:01 +0530

    Hola

---
+++ main.py
@@ -0,0 +1,14 @@
+#value
+tree1 = 98
+tree2 = 24
+tree3 = 65 
+tree4 = 69
+tree5 = 96
+
+#Finnding the total tree
+sum = tree1+tree2+tree3+tree4+tree5
+print("sum of all the 5 tree are :", sum)
+
+#Finding the avarege of tree
+average = sum/5
+print("average of 5 trees are :", average)
